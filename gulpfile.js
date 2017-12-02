9f3e3e72461e6613fd69ed82f5f6b9b335d2da8e
Oles Honchar <borovikoles@gmail.com>
2017-12-02 20:19:11 +0200

    Markup top-line

--- gulpfile.js
+++ gulpfile.js
@@ -28,6 +28,7 @@ gulp.task('common-js', function() {
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/mmenu/js/jquery.mmenu.all.min.js',
 		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))

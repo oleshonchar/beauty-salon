eced6a95c7a7e95d4405989aa469f02761bb1ba4
Oles Honchar <borovikoles@gmail.com>
2017-12-03 20:47:43 +0200

    Add nav

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,21 @@
 $(function() {
 
-	// Custom JS
+	$('#my-menu').mmenu({
+		extensions: ['widescreen', 'theme-black', 'effect-menu-slide', 'pagedim-black'],
+		navbar: {
+			title: '<img src="img/logo-1.svg" alt="S&Mitler">'
+		},
+		offCanvas: {
+			position: 'right'
+		}
+	});
 
+	var api = $('#my-menu').data('mmenu');
+	api.bind('opened', function() {
+		$('.hamburger').addClass('is-active');		
+	})
+	.bind('closed', function() {
+		$('.hamburger').removeClass('is-active');
+	});
+				
 });

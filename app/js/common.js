d84e89484781460282089b4b9d6ca6bbf843975f
Oles Honchar <borovikoles@gmail.com>
2017-12-04 21:32:36 +0200

    Add plugin carousel

--- app/js/common.js
+++ app/js/common.js
@@ -17,5 +17,25 @@ $(function() {
 	.bind('closed', function() {
 		$('.hamburger').removeClass('is-active');
 	});
+
+	$('.carousel-services').owlCarousel({
+		loop: true,
+		nav: true,
+		smartSpeed: 700,
+		navText: ['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-right"></i>'],
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			800: {
+				items: 2
+			},
+			1100: {
+				items: 3
+			}
+		}
+
+	});
 				
 });
--- gulpfile.js
+++ gulpfile.js
@@ -29,6 +29,7 @@ gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/mmenu/js/jquery.mmenu.all.min.js',
+		'app/libs/owl.carousel/owl.carousel.min.js',
 		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
